4fefcb46421ecc3cc151da29a6c3cc67ce7588d8
martinlastname <person@martinlastname.com>
2019-05-27 10:12:27 -0700

    Begun implementing scrollable view of social media posts including custom PostView.

--- app/src/main/java/com/example/avoiddit/MainActivity.java
+++ app/src/main/java/com/example/avoiddit/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.avoiddit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ScrollView mPostList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPostList = findViewById(R.id.postList);
     }
 }
