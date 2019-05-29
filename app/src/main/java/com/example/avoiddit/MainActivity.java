097558f11d32eb3922897ac9eeba23c0f6ae3694
martinlastname <person@martinlastname.com>
2019-05-29 14:16:53 -0700

    Merged with dev to update layout.

--- app/src/main/java/com/example/avoiddit/MainActivity.java
+++ app/src/main/java/com/example/avoiddit/MainActivity.java
@@ -2,18 +2,29 @@ package com.example.avoiddit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView mPostList;
 
+    private String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_main, mobileArray);
+
+        mPostList = findViewById(R.id.postList);
+        mPostList.setAdapter(adapter);
 
 
     }
