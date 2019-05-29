23ef1daf8e84a8dff182da42ffedd64f4de0b6a0
SethFroGo <seth.frogo@gmail.com>
2019-05-29 14:29:12 -0700

    re-moved mainactivity

--- app/src/main/java/com/example/avoiddit/activities/MainActivity.java
+++ app/src/main/java/com/example/avoiddit/activities/MainActivity.java
@@ -2,20 +2,31 @@ package com.example.avoiddit.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.ScrollView;
 
 import com.example.avoiddit.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView mPostList;
 
+    private String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.activity_main, mobileArray);
+
+        mPostList = findViewById(R.id.postList);
+        mPostList.setAdapter(adapter);
 
 
     }
