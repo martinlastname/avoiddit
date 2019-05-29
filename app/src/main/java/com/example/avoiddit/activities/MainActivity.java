a9d6221664c04730112d5f28a111d8dbe50be5c9
martinlastname <person@martinlastname.com>
2019-05-29 14:44:07 -0700

    Cleaned dev of relics from featScroll

--- app/src/main/java/com/example/avoiddit/activities/MainActivity.java
+++ app/src/main/java/com/example/avoiddit/activities/MainActivity.java
@@ -13,21 +13,9 @@ import com.example.avoiddit.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView mPostList;
-
-    private String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
-            "WebOS","Ubuntu","Windows7","Max OS X"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.activity_main, mobileArray);
-
-        mPostList = findViewById(R.id.postList);
-        mPostList.setAdapter(adapter);
-
-
     }
 }
