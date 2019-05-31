66e3ca6783b80e0005c949847862b8ae1458b273
martinlastname <person@martinlastname.com>
2019-05-31 13:00:52 -0700

    Adjusted list to reasonable test dimensions and added items to guarantee scrollability.

--- app/src/main/java/com/example/avoiddit/activities/MainActivity.java
+++ app/src/main/java/com/example/avoiddit/activities/MainActivity.java
@@ -9,17 +9,18 @@ import com.example.avoiddit.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
-            "WebOS","Ubuntu","Windows7","Max OS X"};
+    String[] mobileArray = {"Android", "Linux", "IPhone", "Zune", "WindowsMobile", "Roku",
+            "Blackberry", "PHP", "WebOS", "Padding", "Ubuntu", "Pillow Fortran", "Windows7",
+            "Nintendo Switch", "Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mobileArray);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mobileArray);
 
         ListView postList = findViewById(R.id.postList);
-        postList.setAdapter(myAdapter);
+        postList.setAdapter(adapter);
     }
 }
