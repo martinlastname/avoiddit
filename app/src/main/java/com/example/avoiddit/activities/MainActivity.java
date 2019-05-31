638a38b0f84eb682282a71f9b53a542928983ea5
martinlastname <person@martinlastname.com>
2019-05-31 08:46:02 -0700

    Revert "Pull reddit post BUT cannot build due to hamcrest junit duplication"
    This reverts commit 484d9743744b53de86c9b40d1a20b1c6ec445ae6.

--- app/src/main/java/com/example/avoiddit/activities/MainActivity.java
+++ app/src/main/java/com/example/avoiddit/activities/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.avoiddit.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
@@ -12,11 +10,6 @@ import android.widget.ListView;
 import android.widget.ScrollView;
 
 import com.example.avoiddit.R;
-import com.example.avoiddit.model.RedditFetcher;
-import com.github.jreddit.entity.Submission;
-import com.github.jreddit.retrieval.Submissions;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,15 +17,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button fetchRedButton = findViewById(R.id.testerButton);
-        fetchRedButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                RedditFetcher testFetch = new RedditFetcher("IAmYourVader", "legoland123");
-                List<Submission> postsList = testFetch.getTenPosts("ProgrammerHumor");
-                System.out.println(postsList);
-            }
-        });
     }
 }
--- app/src/main/java/com/example/avoiddit/model/RedditBlock.java
+++ app/src/main/java/com/example/avoiddit/model/RedditBlock.java
@@ -4,7 +4,4 @@ import java.io.Serializable;
 
 public class RedditBlock extends PostBlock implements Serializable {
 
-    public RedditBlock() {
-        super();
-    }
 }
