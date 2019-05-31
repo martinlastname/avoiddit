03bf472f337b8fa06aefcfc1f4a4512754c208de
martinlastname <person@martinlastname.com>
2019-05-31 12:52:03 -0700

    Implemented simple scrollable list of strings.

--- app/src/main/java/com/example/avoiddit/activities/MainActivity.java
+++ app/src/main/java/com/example/avoiddit/activities/MainActivity.java
@@ -3,19 +3,23 @@ package com.example.avoiddit.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.ScrollView;
-
 import com.example.avoiddit.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mobileArray);
+
+        ListView postList = findViewById(R.id.postList);
+        postList.setAdapter(myAdapter);
     }
 }
