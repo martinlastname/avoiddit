01443659a14069526883fffb9b88eae161fd57f7
martinlastname <person@martinlastname.com>
2019-06-03 15:46:26 -0700

    Scrolling list view now uses custom adapter.

--- app/src/main/java/com/example/avoiddit/PostListAdapter.java
+++ app/src/main/java/com/example/avoiddit/PostListAdapter.java
@@ -5,18 +5,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.avoiddit.model.PostBlock;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class PostListAdapter extends ArrayAdapter<PostBlock> {
+// TODO change to <PostBlock>
+public class PostListAdapter extends ArrayAdapter<String> {
 
     private Context mContext;
+    private List<String> contentList;
 
-
-    public PostListAdapter(Context context, int resource, List objects) {
-        super(context, resource, objects);
+    public PostListAdapter(Context context, int resource, ArrayList<String> list) {
+        super(context, resource, list);
+        mContext = context;
+        contentList = list;
     }
 
     public void add(PostBlock object){
@@ -27,8 +34,11 @@ public class PostListAdapter extends ArrayAdapter<PostBlock> {
     public View getView(int position, View convertView, ViewGroup parent){
         View listItem = convertView;
         if(listItem == null){
-            listItem = LayoutInflater.from(mContext).inflate(R.layout.post_layout, parent, false);
+            listItem = LayoutInflater.from(mContext).inflate(R.layout.layout_post, parent, false);
         }
+
+        TextView title = (TextView) listItem.findViewById(R.id.textView_Title);
+        title.setText(contentList.get(position));
 
 
         return listItem;
--- app/src/main/java/com/example/avoiddit/activities/MainActivity.java
+++ app/src/main/java/com/example/avoiddit/activities/MainActivity.java
@@ -5,20 +5,25 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.example.avoiddit.PostListAdapter;
 import com.example.avoiddit.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] mobileArray = {"Android", "Linux", "IPhone", "Zune", "WindowsMobile", "Roku",
+    ArrayList<String> mobileArray = new ArrayList<String>(Arrays.asList("Android", "Linux", "IPhone", "Zune", "WindowsMobile", "Roku",
             "Blackberry", "PHP", "WebOS", "Padding", "Ubuntu", "Pillow Fortran", "Windows7",
-            "Nintendo Switch", "Max OS X"};
+            "Nintendo Switch", "Max OS X"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mobileArray);
+        PostListAdapter adapter = new PostListAdapter(this, android.R.layout.simple_list_item_1, mobileArray);
 
         ListView postList = findViewById(R.id.postList);
         postList.setAdapter(adapter);
