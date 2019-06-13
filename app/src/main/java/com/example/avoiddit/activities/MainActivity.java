5830a8162862067a6218155e99f885bbb937d88e
martinlastname <person@martinlastname.com>
2019-06-13 12:49:02 -0700

    Fixed typo in key constants for Post Page intent.

--- app/src/main/java/com/example/avoiddit/activities/MainActivity.java
+++ app/src/main/java/com/example/avoiddit/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.avoiddit.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,7 +18,7 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
     // Constants for use as keys when adding data to Intents.
     public static final String EXTRA_TITLE = "com.example.avoiddit.TITLE";
-    public static final String EXTRA_CONTENT = "com.example.avoiddit.TITLE";
+    public static final String EXTRA_CONTENT = "com.example.avoiddit.CONTENT";
 
     ArrayList<String> testMobileArray = new ArrayList<String>(Arrays.asList("Android", "Linux", "IPhone", "Zune", "WindowsMobile", "Roku",
             "Blackberry", "PHP", "WebOS", "Padding", "Ubuntu", "Pillow Fortran", "Windows7",
@@ -47,7 +48,9 @@ public class MainActivity extends AppCompatActivity {
     public void onPostItemClick(View view){
         Intent intent = new Intent(this, PostPageActivity.class);
         TextView title = view.findViewById(R.id.textView_postList_Title);
+        Log.d("TITLE ---", title.getText().toString());
         TextView content = view.findViewById(R.id.textView_postList_Content);
+        Log.d("CONTENT ---", content.getText().toString());
         intent.putExtra(EXTRA_TITLE, title.getText().toString());
         intent.putExtra(EXTRA_CONTENT, content.getText().toString());
         startActivity(intent);
--- app/src/main/java/com/example/avoiddit/activities/PostPageActivity.java
+++ app/src/main/java/com/example/avoiddit/activities/PostPageActivity.java
@@ -3,6 +3,7 @@ package com.example.avoiddit.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.avoiddit.R;
@@ -15,7 +16,11 @@ public class PostPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post_page);
 
         Intent intent = getIntent();
-        String body = intent.getStringExtra(MainActivity.EXTRA_TITLE) + "\n" + intent.getStringExtra(MainActivity.EXTRA_CONTENT);
+        String title = intent.getStringExtra(MainActivity.EXTRA_TITLE);
+        Log.d("ACTIVITY TITLE ---", title);
+        String content = intent.getStringExtra(MainActivity.EXTRA_CONTENT);
+        Log.d("ACTIVITY CONTENT ---", content);
+        String body = title + "\n" + content;
         TextView textView = findViewById(R.id.textView_postActivity_title);
         textView.setText(body);
     }
