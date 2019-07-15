a11dd67e2cd979250d8114122063f2b70ec234b8
martinlastname <person@martinlastname.com>
2019-07-14 18:47:00 -0700

    Changing from Mastodon4j to direct interaction with Mastodon's Rest API.

---
+++ app/src/main/java/com/example/avoiddit/UrlGetter.java
@@ -0,0 +1,26 @@
+package com.example.avoiddit;
+
+import android.os.AsyncTask;
+
+public class UrlGetter extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected void onPreExecute(){
+
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values){
+
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+    }
+}
